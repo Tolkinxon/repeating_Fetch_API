ee4dece484753777616acbf762e737db59d6af9b
Tolkinxon <krosstony639@gmail.com>
2022-12-12 18:29:33 +0500

    changed todos

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
 
-fetch('https://jsonplaceholder.typicode.com/todos')
+fetch('https://jsonplaceholder.typicode.com/todos/4')
   .then(response => response.json())
   .then(json => console.log(json))
